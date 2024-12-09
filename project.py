707642b5400aa33d3aea448a4bdd6436899995ba
KRUPALIVIRANI14 <krupalivirani55@gmail.com>
2024-12-09 17:27:02 +0530

    hello

---
+++ project.py
@@ -0,0 +1,15 @@
+a = int(input("enter the first number:-"))
+b = int(input("enter the second number:-"))
+print("1.+"
+      "2.-"
+      "3.*"
+      "4./")
+c = int(input("entr the op:"))
+if c==1:
+    print(a+b)
+elif c==2:
+    print(a-b)
+elif c==3:
+    print(a*b)
+elif c==4:
+    print(a/b)
